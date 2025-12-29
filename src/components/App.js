d2da424cafc13e93cb85e73ae7526c40717e287e
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 14:45:50 +0530

    Add login functionality to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import LoginForm from "./LoginForm";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
 
-export default App
+      {isLoggedIn ? (
+        <p>You are logged in</p>
+      ) : (
+        <LoginForm
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+        />
+      )}
+    </div>
+  );
+};
